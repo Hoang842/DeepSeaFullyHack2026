b97fc7f04d6ccb612c245d2217037ab8c0a5d1ee
Hoang842 <85919003+Hoang842@users.noreply.github.com>
2026-04-18 22:06:47 -0700

    Update

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    private Vector3 originalScale;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        originalScale = transform.localScale; // lưu scale ban đầu
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement = movement.normalized;
+
+        // Flip đúng cách
+        if (movement.x > 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+        else if (movement.x < 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement * moveSpeed;
+    }
+}
