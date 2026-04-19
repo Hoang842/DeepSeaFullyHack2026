1fc4a56d238dd52641221f4e0c23c6fa0136e88f
Hoang842 <85919003+Hoang842@users.noreply.github.com>
2026-04-19 00:15:43 -0700

    Update

---
+++ Assets/Script/OxygenBarUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OxygenBarUI : MonoBehaviour
+{
+    public PlayerController player;
+    public Image fillImage;
+
+    void Update()
+    {
+        if (player != null && fillImage != null)
+        {
+            fillImage.fillAmount = player.currentOxygen / player.maxOxygen;
+        }
+    }
+}
